0639e6e9fb652898a20e142d91c3f58e22363a8c
Marcus Speight <marcus.speight@hotmail.co.uk>
2026-08-17 08:36:08 +0100

    Added ArgoCD

---
+++ argocd/boot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+kubectl create --context="k3d-cluster1" namespace argocd
+kubectl apply --context="k3d-cluster1" -k argocd -n argocd --server-side --force-conflicts
+
+ADMIN_PASSWORD=$(kubectl get --context="k3d-cluster1" secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
+echo "login with at https://argocd.docker.localhost"
+echo "username: admin"
+echo "password: $ADMIN_PASSWORD"
+echo ""
+echo "Please change the password ASAP"
