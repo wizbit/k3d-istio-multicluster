c6ebd6205fc8406799edc5d6acaec52e98993ddf
Marcus Speight <marcus.speight@hotmail.co.uk>
2026-08-17 08:36:02 +0100

    Istio connects and syncs Traefik does not connect through

--- install-istio.sh
+++ install-istio.sh
@@ -9,6 +9,24 @@ prelim_istio() {
   make -f ./Makefile.selfsigned.mk cluster2-cacerts
 }
 
+retry() {
+  local retries="$1" # First argument
+  local command="$2" # Second argument
+
+  # Run the command, and save the exit code
+  $command
+  local exit_code=$?
+
+  # If the exit code is non-zero (i.e. command failed), and we have not
+  # reached the maximum number of retries, run the command again
+  if [[ $exit_code -ne 0 && $retries -gt 0 ]]; then
+    retry $(($retries - 1)) "$command"
+  else
+    # Return the exit code from the command
+    return $exit_code
+  fi
+}
+
 install_istio() {
   cluster=$1
   context="k3d-$cluster"
@@ -17,24 +35,55 @@ install_istio() {
   kubectl create --context "${context}" namespace istio-system
   kubectl label --context "${context}" namespace istio-system "topology.istio.io/network=${network}"
 
-  kubectl create --context "${context}" secret generic cacerts -n istio-system --save-config --dry-run=client \
+  kubectl create --context "${context}" secret generic cacerts -n istio-system --dry-run=client \
         --from-file="${cluster}/ca-cert.pem" \
         --from-file="${cluster}/ca-key.pem" \
         --from-file="${cluster}/root-cert.pem" \
         --from-file="${cluster}/cert-chain.pem" \
-        -o yaml | \
-        kubectl apply -f -
+        -o yaml | kubectl --context "${context}" apply -f -
 
-  helm upgrade --install istio-base istio/base -n istio-system --kube-context "${context}" --wait
-  helm upgrade --install istiod istio/istiod -n istio-system --kube-context "${context}" --set global.meshID=mesh1 --set global.multiCluster.clusterName="${cluster}" --set global.network="${network}" --set profile=ambient --set env.AMBIENT_ENABLE_MULTI_NETWORK="true" --set env.AMBIENT_ENABLE_BAGGAGE="true" --wait
-  helm upgrade --install istio-cni istio/cni -n istio-system --kube-context "${context}" --set profile=ambient --set global.platform=k3d --wait
+  cat <<EOF | istioctl install --context="${context}" -y -f -
+apiVersion: insall.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  profile: ambient
+  components:
+    pilot:
+      k8s:
+        env:
+          - name: AMBIENT_ENABLE_MULTI_NETWORK
+            value: "true"
+          - name: AMBIENT_ENABLE_BAGGAGE
+            value: "true"
+  values:
+    global:
+      platform: k3d
+      meshID: mesh1
+      multiCluster:
+        clusterName: ${cluster}
+      network: ${network}
+EOF
 
-  helm upgrade --install ztunnel istio/ztunnel -n istio-system --kube-context "${context}" --set multiCluster.clusterName="${cluster}" --set global.network="${network}" --wait
 
-  ./gen-eastwest-gateway.sh \
-    --network "${network}" \
-    --ambient | \
-    kubectl --context="${context}" apply -f -
+cat <<EOF | kubectl apply --context="${context}" -f -
+kind: Gateway
+apiVersion: gateway.networking.k8s.io/v1
+metadata:
+  name: istio-eastwestgateway
+  namespace: istio-system
+  labels:
+    topology.istio.io/network: "${network}"
+spec:
+  gatewayClassName: istio-east-west
+  listeners:
+  - name: mesh
+    port: 15008
+    protocol: HBONE
+    tls:
+      mode: Terminate # represents double-HBONE
+      options:
+        gateway.istio.io/tls-terminate-mode: ISTIO_MUTUAL
+EOF
 }
 
 prelim_istio
@@ -45,14 +94,29 @@ install_istio cluster2 network2
 cluster1IP=$(docker inspect k3d-cluster1-server-0 | jq -r '.[0].NetworkSettings.Networks."k3d-mesh".IPAddress')
 cluster2IP=$(docker inspect k3d-cluster2-server-0 | jq -r '.[0].NetworkSettings.Networks."k3d-mesh".IPAddress')
 
+for cluster in cluster1 cluster2; do
+  context="k3d-${cluster}"
+  # Manually create a long-lived token secret for Istio
+  kubectl apply --context "${context}" -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: istio-reader-service-account-token
+  namespace: istio-system
+  annotations:
+    kubernetes.io/service-account.name: istio-reader-service-account
+type: kubernetes.io/service-account-token
+EOF
+done
+
 istioctl create-remote-secret \
   --context="k3d-cluster1" \
   --name=cluster1 \
-  --server="http://${cluster1IP}:6443" | \
+  --server="https://${cluster1IP}:6443" | \
   kubectl apply -f - --context="k3d-cluster2"
 
 istioctl create-remote-secret \
   --context="k3d-cluster2" \
   --name=cluster2 \
-  --server="http://${cluster2IP}:6443" | \
+  --server="https://${cluster2IP}:6443" | \
   kubectl apply -f - --context="k3d-cluster1"
